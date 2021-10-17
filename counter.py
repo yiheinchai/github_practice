a754b34d728bd79f8d990c496249b03c5f6179d6
Yi Hein Chai <chaiyihein@gmail.com>
2021-10-17 15:40:50 +0100

    Deleted some files

--- counter.py
+++ counter.py
@@ -4,6 +4,7 @@ while user_input_valid == False:
     counter = int(input("Enter the number to start: "))
     if counter < 5:
         user_input_valid = True
+        print("Your input is valid!")
 
 while counter <= 5:
     print("hello", counter)
