2229fa5c330ea591bdaaee86fc8dc0ea109fb0a8
Yi Hein Chai <76833604+yiheinchai@users.noreply.github.com>
2021-10-17 16:09:29 +0100

    Smiley face!

--- counter.py
+++ counter.py
@@ -4,7 +4,7 @@ while user_input_valid == False:
     counter = int(input("Enter the number to start: "))
     if counter < 5:
         user_input_valid = True
-        print("Your input is valid!")
+        print("Your input is valid! Yay!")
 
 while counter <= 5:
     print("hello", counter)
